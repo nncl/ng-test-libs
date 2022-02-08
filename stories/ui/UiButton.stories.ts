2ad8d0e4684ce8c32d73e4fce5ae000733c25448
Cauê Almeida <nncl@live.com>
2022-02-08 15:19:27 -0300

    feat(storybook): Documenting default button

--- stories/ui/UiButton.stories.ts
+++ stories/ui/UiButton.stories.ts
@@ -2,20 +2,25 @@ import { Story, Meta } from '@storybook/angular/types-6-0';
 import { UiComponent } from '../../projects/ui/src/lib/ui.component';
 
 export default {
-  title: 'UI/Button',
+  title: 'VSS-UI/Button',
   component: UiComponent,
+  argTypes: {
+    slot: {
+      description: 'CTA label',
+    }
+  }
 } as Meta;
 
 const Template: Story<UiComponent> = (args: UiComponent) => ({
   props: args,
   template: `
     <vss-ui>
-      {{ ngContent }}
+      {{ slot }}
     </vss-ui>
   `
 });
 
-export const Primary = Template.bind({});
-Primary.args = {
-  'ngContent': 'Click me!'
+export const Default = Template.bind({});
+Default.args = {
+  slot: 'Click me!'
 };
